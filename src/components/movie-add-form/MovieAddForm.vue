e9746ea338b05159d68eba8add09fb987e2a24c4
don1yorbe <mirkomilovdoniyor4@gmail.com>
2023-11-09 19:34:43 +0500

    add new files

---
+++ src/components/movie-add-form/MovieAddForm.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <button 
+        type="button" 
+        class="btn btn-primary" 
+        data-bs-toggle="modal" 
+        data-bs-target="#exampleModal" 
+        data-bs-whatever="@mdo">Add List
+        </button>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">New List</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <form>
+                  <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">List name:</label>
+                    <Input
+                    class="new-movie-label" 
+                    placeholder="List name" 
+                    v-model="name"
+                    @input="name = $event.target.value"/>
+                  </div>
+                  <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">Longitude:</label>
+                    <Input
+                    class="new-movie-label"
+                    placeholder="Longitude"
+                    type="number"
+                    v-model="viewers"
+                    @input="viewers = $event.target.value"/>
+                  </div>
+                  <!-- <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">Latitude:</label>
+                    <Input
+                    class="new-movie-label"
+                    placeholder="Nechi marotaba ko'rilgan?"
+                    type="number"
+                    v-model="viewers"
+                    @input="viewers = $event.target.value"/>
+                  </div> -->
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <PrimaryButton 
+                class="btn-outline-dark" 
+                type="submit" 
+                @click="addMovie">Save</PrimaryButton>
+              </div>
+            </div>
+          </div>
+         </div>
+    </div>
+</template>
+
+<script>
+import Input from "@/ui-components/Input.vue";
+
+export default {
+    components: {Input},
+    data(){
+        return{
+            name: '',
+            viewers: '',
+        }
+    },
+    methods: {
+        addMovie() {
+            if(!this.name || !this.viewers) return
+            const newMovie = {
+                name: this.name,
+                viewers: this.viewers,
+                favourite: false,
+                like: false,
+            }
+            this.$emit('createMovie', newMovie) //ona divga ma'lumot jo'natish
+            this.name = ''
+            this.viewers = ''
+        },
+    },
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/search-panel/SearchPanel.vue
@@ -0,0 +1,42 @@
+<template>
+    <Input
+    class="search-input"
+    placeholder="Search List"
+    v-model="term"
+    @input="changeHandler"/>
+</template>
+
+<script>
+import Input from "@/ui-components/Input.vue";
+
+export default {
+    components: {Input},
+  props: {
+    updateTernHandler: {
+      type: Function,
+      required: true,
+    },
+  },
+  data() {
+    return{
+      term: '',
+    }
+  },
+  methods: {
+    changeHandler(e){
+      this.term = e.target.value
+      this.updateTernHandler(this.term)
+    },
+  },
+}
+</script>
+
+<style scoped>
+  .search-input{
+    margin-bottom: 1rem;
+    padding: 1.5rem;
+    background-color: #fcfaf5;
+    border-radius: 4px;
+    box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+  }
+</style>
